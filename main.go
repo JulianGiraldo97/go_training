c32d83c34b6227f7a23ae9b2d6bb213d845159f4
Julian Giraldo <julian09426@gmail.com>
2024-06-08 19:07:34 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "hello_world/variables"
+
+func main() {
+
+	variables.ShowIntegers()
+
+}
---
+++ variables/variables.go
@@ -0,0 +1,13 @@
+package variables
+
+import (
+	"fmt"
+)
+
+func ShowIntegers() {
+	var normalInt int
+	intde32 := int32(10)
+
+	fmt.Println("int normal:", normalInt, "int de 32:", intde32)
+
+}
