635afdac4a5a16d18f3e6546d3578c597e83f16c
Julian Giraldo <julian09426@gmail.com>
2024-06-26 08:51:19 -0500

    condiciones if

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"hello_world/variables"
+	"runtime"
 )
 
 func main() {
 	estado, texto := variables.ConviertoaTexto(1558)
 	fmt.Print(estado)
 	fmt.Print(texto)
+	if os := runtime.GOOS; os == "windows" || os == "OS X." {
+		fmt.Println("Esto no es windows, es", os)
+	} else {
+		fmt.Println("Esto es windows, es", os)
+	}
 }
